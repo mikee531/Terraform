bd18be741300463520e50f204f03540f83a4b547
mikee531 <30585083+mikee531@users.noreply.github.com>
2023-08-23 15:27:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,25 @@
-provider "aws" {
-  region = "us-east-1"  # Replace with your desired AWS region
+# use ubuntu 20 AMI for EC2 instance
+data "aws_ami" "ubuntu" {
+    most_recent = true
+filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/*20.04-amd64-server-*"]
+    }
+filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+owners = ["099720109477"] # Canonical
 }
-
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Free-tier Amazon Linux 2 AMI ID
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleInstance"
+# provision to us-east-2 region
+provider "aws" {
+  region  = "us-east-2"
+}
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name      = "app-ssh-key"
+tags = {
+    Name = var.ec2_name
   }
 }
